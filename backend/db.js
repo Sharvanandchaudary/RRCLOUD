70f1cef6eaa0a1c093a55560534edb6d4967a8e0
Veera Venkata Saravanakumar <vsaravan@cadence.com>
2025-12-21 03:21:45 -0800

    docker

--- backend/db.js
+++ backend/db.js
@@ -1,19 +1,12 @@
 const { Pool } = require('pg');
-require('dotenv').config();
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
+  host: process.env.DB_HOST, // /cloudsql/...
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: 5432,
+  ssl: false
 });
 
-pool.on('connect', () => {
-  console.log('✅ Database Connected!');
-});
-
-pool.on('error', (err) => {
-  console.error('❌ Database Error:', err.message);
-});
-
-module.exports = {
-  query: (text, params) => pool.query(text, params),
-};
+module.exports = pool;
